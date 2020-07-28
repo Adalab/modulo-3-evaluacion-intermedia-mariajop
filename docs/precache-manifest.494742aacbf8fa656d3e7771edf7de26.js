e03a01c2bf106526e69cd9040a3762852ddcad0f
mariajop <mariajops@gmail.com>
2020-07-28 09:01:41 +0200

    publishing to github pages

---
+++ docs/precache-manifest.494742aacbf8fa656d3e7771edf7de26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d613813887feb918988d0dfb75aced3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "820e978f27d80dfca305",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4018d3c44fd04e2279ab",
+    "url": "./static/js/2.0e997a9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0e997a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820e978f27d80dfca305",
+    "url": "./static/js/main.e265214a.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/media/logo.d41d8cd9.svg"
+  }
+]);
